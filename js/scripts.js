9693e2c3eb8d91b3e4b0b5ab013d5281087a34be
Daren Schaad and Neil Larion <darenschaad@gmail.com>
2016-01-13 15:11:44 -0800

    Add array of ice cream flavors and foreach loop to display on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 $(function(){
-  //something here
+  var array = ["Cookie Dough","Chocolate","Caramel Swirl","Boom Chocolatta","Hazed &amp; Confused","Karamel Sutra", "Americone Dream", "Cherry Garcia"];
+  array.forEach(function(flavor){
+    $(".iceCream").append("<li>"+flavor+"</li>")
+  });
 
 });
