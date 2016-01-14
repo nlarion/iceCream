a8a5e826d4867d05e6dc32bdcf2482e786b73086
Daren Schaad and Neil Larion <neil.larion@gmail.com>
2016-01-13 16:32:51 -0800

    Update readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(function(){
   var array = ["Cookie Dough","Chocolate","Caramel Swirl","Boom Chocolatta","Hazed &amp; Confused","Karamel Sutra", "Americone Dream", "Cherry Garcia"];
 
-  for (var i = 0; i < array.length; i++){
+  array.forEach(function(flavor){
+    $(".iceCream").append("<li>"+flavor+"</li>")
+  });
+
+  for (var i = 0; i < array.length +1; i++){
     $(".iceCream").append("<li>"+array[i]+"</li>")
   }
 
